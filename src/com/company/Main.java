11930eb3e76761aa7076496bc1752d926c1eef20
cjt5047 <1297338646@qq.com>
2018-03-13 13:23:43 +0800

    资讯网站练手

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	    System.out.println("Hello world!");
         System.out.println("Hello world2!");
         System.out.println("Hello world!");
+        System.out.println("hello");
     }
 
 }
